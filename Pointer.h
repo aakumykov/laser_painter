8b59fa59694e838006fc8b1841559be452822826
Андрей Кумыков <aakumykov@yandex.ru>
2016-11-15 22:22:44 +0700

    pointer.drawLine(), pointer.drawPoint()

--- Pointer.h
+++ Pointer.h
@@ -11,12 +11,34 @@ class Pointer
       this->currentX = POINTER_START_X;
       this->currentY = POINTER_START_Y;
     }
+    
     void init(){
         Msg::debug("Pointer.init()");
       this->laser.init();
       this->dac.init();
       this->goTo(this->currentX, this->currentY);
     }
+    
+    void drawLine(int x1, int y1, int x2, int y2){
+      Msg::debug("Pointer.drawLine("+String(x1)+","+String(y1)+","+String(x2)+","+String(y2)+")");
+      this->goTo(x1,y1);
+      this->lineTo(x2,y2);
+    }
+    
+    void drawPoint(int x, int y){
+      Msg::debug("Pointer.drawPoint("+String(x)+","+String(y)+")");
+      this->goTo(x,y);
+      this->laser.on();
+      delayMicroseconds(100);
+      this->laser.off();
+      delayMicroseconds(50);
+    }
+
+  private:
+    Laser laser;
+    DoubleDAC dac;
+    int currentX, currentY;
+    
     void goTo(int x, int y){
         Msg::debug("Pointer.goTo("+String(x)+","+String(y)+")");
       int timeout = round(distanceTo(x,y) / POINTER_SPEED);
@@ -25,6 +47,7 @@ class Pointer
       this->dac.write(x,y);
       delayMicroseconds(timeout);
     }
+    
     void lineTo(int x, int y){
         Msg::debug("Pointer.lineTo("+String(x)+","+String(y)+")");
       int timeout = round(distanceTo(x,y) / POINTER_SPEED);
@@ -34,19 +57,7 @@ class Pointer
       delayMicroseconds(timeout);
       this->laser.off();
     }
-  
-    void drawLine(int x1, int y1, int x2, int y2){
-      Msg::debug("Pointer.drawLine("+String(x1)+","+String(y1)+","+String(x2)+","+String(y2)+")");
-      
-    }
-    void drawPoint(int x, int y){
-      Msg::debug("Pointer.drawPoint("+String(x)+","+String(y)+")");
-      
-    }
-  private:
-    Laser laser;
-    DoubleDAC dac;
-    int currentX, currentY;
+    
     float distance(int x1, int y1, int x2, int y2){
         Msg::debug("Pointer.distance("+String(x1)+","+String(y1)+","+String(x2)+","+String(y2)+")");
       return sqrt( (x2-x1)^2 + (y2-y1)^2 );
@@ -56,5 +67,6 @@ class Pointer
       // дублирование кода ради ускорения выполнения
       return sqrt( (x - this->currentX)^2 + (y - this->currentY)^2 );
     }
+
 };
 
