c79db808067be838087660f3c5059e3588e2f540
Андрей Кумыков <aakumykov@yandex.ru>
2016-11-15 22:20:30 +0700

    Задержки на включение и выключение лазера.

--- Laser.h
+++ Laser.h
@@ -7,10 +7,12 @@ class Laser {
     void on(){
         Msg::debug("Laser.on()");
       digitalWrite(LASER_PIN, HIGH);
+      delayMicroseconds(5);
     }
     void off(){
         Msg::debug("Laser.off()");
       digitalWrite(LASER_PIN,LOW);
+      delayMicroseconds(5);
     }
 };
 
